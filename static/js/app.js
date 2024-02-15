5159e4bb841315395afe1143a19bb627ed506e86
bryanpijohnson <47281061+bryanpijohnson@users.noreply.github.com>
2024-02-15 07:59:30 -0500

    bar chart and bubble graph done

--- static/js/app.js
+++ static/js/app.js
@@ -8,7 +8,10 @@ function init() {
     names.forEach(name => {
       dropdown.append("option").text(name).property("value", name);
     });
-    demographics(names[0])
+    demographics(names[0]);
+    barChart(names[0]);
+    gaugeChart(names[0]);
+    bubbleChart(names[0]);
   });
 };
 
@@ -19,7 +22,7 @@ function demographics(ids) {
       return person.id == ids;
     }
     let meta = data.metadata.filter(findIdData)[0];
-    console.log(meta);
+    //console.log(meta);
     let text = `id: ${meta.id}\n
       ethnicity: ${meta.ethnicity}\n
       gender: ${meta.gender}\n
@@ -33,7 +36,36 @@ function demographics(ids) {
 }
 
 function barChart(ids) {
+  d3.json(url).then(function(data){
+    function findIdData(person) {
+      return person.id == ids;
+    }
+    let samples = data.samples.filter(findIdData)[0];
+    let values = samples.sample_values.slice(0,10).reverse();
+    let otuIds = samples.otu_ids.slice(0,10).reverse();
+    let labels = samples.otu_labels.slice(0,10).reverse();
+    
+    let trace1 = {
+      x: values,
+      y: otuIds.map(item => "OTU " + item),
+      text: labels,
+      type: "bar",
+      orientation: "h"
+    };
 
+    let layout = {
+      margin: {
+        l: 100,
+        r: 100,
+        t: 0,
+        b: 100
+      }
+    };
+
+    let data1 = [trace1];
+
+    Plotly.newPlot("bar", data1, layout);
+  });
 };
 
 function gaugeChart(ids) {
@@ -41,7 +73,34 @@ function gaugeChart(ids) {
 };
 
 function bubbleChart(ids) {
+  d3.json(url).then(function(data){
+    function findIdData(person) {
+      return person.id == ids;
+    };
+    let samples = data.samples.filter(findIdData)[0];
+    console.log(samples);
 
+    var trace1 = {
+      x: samples.otu_ids,
+      y: samples.sample_values,
+      text: samples.otu_labels,
+      mode: "markers",
+      marker: {
+        size: samples.sample_values,
+        color: samples.otu_ids
+      }
+    }
+
+    var data1 = [trace1];
+
+    var layout = {
+      xaxis: {title: "OTU ID"},
+      showlegend: false,
+      height: 600
+    };
+
+    Plotly.newPlot("bubble", data1, layout);
+  })
 };
 
 
